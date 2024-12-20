8f7f0c3e90d7cdbe7ea596eb127181980bc2df42
ChHsiching <hsichingchang@gmail.com>
2024-12-20 15:16:49 +0800

    ✨ Feat: demo for generating dynamic files
    Using FreeMarker Template Engine to Generate Dynamic Files

--- hsipig-generator-basic/src/main/java/com/hsipig/Main.java
+++ hsipig-generator-basic/src/main/java/com/hsipig/Main.java
@@ -12,11 +12,11 @@ public class Main {
         String projectPath = System.getProperty("user.dir");
 
         File parentFile = new File(projectPath).getParentFile();
-//        System.out.println(parentFile);
+        // System.out.println(parentFile);
         // 输入路径：ACM 示例代码模板目录
         String inputPath = new File(parentFile, "hsipig-generator-demo-projects/acm-template").getAbsolutePath();
         // 调整路径
-//        String inputPath = new File("hsipig-generator-demo-projects/acm-template").getAbsolutePath();
+        // String inputPath = new File("hsipig-generator-demo-projects/acm-template").getAbsolutePath();
         // 输出路径：直接输出到项目的根目录
         String outputPath = projectPath;
 
