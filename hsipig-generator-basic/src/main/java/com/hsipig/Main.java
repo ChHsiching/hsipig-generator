5f7c7fb426480d1f22950c6090ffc2ffe3a9b147
ChHsiching <hsichingchang@gmail.com>
2024-12-20 18:41:09 +0800

    ✨ Feat: generate code by calling commands through Picocli
    Temporarily test demo through Main entry function parameters

--- hsipig-generator-basic/src/main/java/com/hsipig/Main.java
+++ hsipig-generator-basic/src/main/java/com/hsipig/Main.java
@@ -1,25 +1,19 @@
 package com.hsipig;
 
-import java.io.File;
-
-import static com.hsipig.generator.StaticGenerator.copyFilesByHutool;
+import com.hsipig.cli.CommandExecutor;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
-        // 获取整个项目的根路径
-        String projectPath = System.getProperty("user.dir");
+        // 模拟执行 generate 子命令，并传递 -l、-a、-o 参数
+//        args = new String[]{"generate", "-l", "-a", "-o"};
+//        args = new String[]{"config"};
+//        args = new String[]{"list"};
 
-        File parentFile = new File(projectPath).getParentFile();
-        // System.out.println(parentFile);
-        // 输入路径：ACM 示例代码模板目录
-        String inputPath = new File(parentFile, "hsipig-generator-demo-projects/acm-template").getAbsolutePath();
-        // 调整路径
-        // String inputPath = new File("hsipig-generator-demo-projects/acm-template").getAbsolutePath();
-        // 输出路径：直接输出到项目的根目录
-        String outputPath = projectPath;
-
-        copyFilesByHutool(inputPath, outputPath);
+        CommandExecutor commandExecutor = new CommandExecutor();
+        commandExecutor.doExecute(args);
     }
+
 }
