0e5919d4e298a7316c86047ca47d986b7718fd87
ChHsiching <hsichingchang@gmail.com>
2024-12-20 14:05:19 +0800

    ✨ Feat: demo for generating static files
    Implemented a copy function using the Hutool library that can directly copy static files to a specified directory

---
+++ hsipig-generator-basic/src/main/java/com/hsipig/Main.java
@@ -0,0 +1,25 @@
+package com.hsipig;
+
+import java.io.File;
+
+import static com.hsipig.generator.StaticGenerator.copyFilesByHutool;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // 获取整个项目的根路径
+        String projectPath = System.getProperty("user.dir");
+
+        File parentFile = new File(projectPath).getParentFile();
+//        System.out.println(parentFile);
+        // 输入路径：ACM 示例代码模板目录
+        String inputPath = new File(parentFile, "hsipig-generator-demo-projects/acm-template").getAbsolutePath();
+        // 调整路径
+//        String inputPath = new File("hsipig-generator-demo-projects/acm-template").getAbsolutePath();
+        // 输出路径：直接输出到项目的根目录
+        String outputPath = projectPath;
+
+        copyFilesByHutool(inputPath, outputPath);
+    }
+}
